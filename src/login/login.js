a1680319cc92228d03fe16bf50463ed6f6337431
martin.takacs <junacik99@gmail.com>
2022-07-06 20:33:18 +0200

    check form data with mocked data

--- src/login/login.js
+++ src/login/login.js
@@ -1,53 +1,69 @@
 import React from "react";
-import { useForm, Controller } from "react-hook-form";
-import { Input } from "@mui/material";
+import { useForm } from "react-hook-form";
 import TextField from '@mui/material/TextField';
 import { Box } from "@mui/system";
+import Container from "@mui/material/Container";
 
 
 export default function Login() {
-    const { control, register, handleSubmit, watch, formState: { errors } } = useForm({
-        defaultValues: {
-            username: '',
-            password: ''
-        }
-    });
-    const onSubmit = data => console.log(data)
+    const {  register, handleSubmit } = useForm();
+    
+    const mocked_user = {
+        username: "admin",
+        pwd: "0000",
+    };
 
+    // Check with mock data
+    const onSubmit = (data) => {
+        console.log(data);
+        if(data.username === mocked_user.username && data.pwd === mocked_user.pwd){
+            console.log("LOGGED IN");
+            /* TODO: transfer to other page */
+        }
+        else{
+            console.log("Incorrect username or password");
+            /* TODO: alert user about incorrect input */
+        }
+    };
 
     return (
-        <Box
-            component="form"
-            sx={{
-                '& .MuiTextField-root': { m: 1, width: '25ch' },
-            }}
-            noValidate
-            autoComplete="off"
-        >
-            <div>
+        <Container maxWidth="xs">
+            <h1>Login Page</h1>
+            <Box
+                sx={{
+                    '& .MuiTextField-root': { m: 2, width: '25ch' },
+                }}
+                noValidate
+                autoComplete="off"
+            >
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <div>
-                        <TextField
-                            required
-                            id="username"
-                            label="Username"
-                            variant="outlined"
-                        />
-                        <TextField
-                            required
-                            id="pwd"
-                            label="Password"
-                            type="password"
-                            variant="outlined"
-                            autoComplete="current-password"
-                        />
+                        <div>
+                            <TextField
+                                required
+                                id="username"
+                                label="Username"
+                                variant="outlined"
+                                fullWidth
+                                {...register("username")}
+                            />
+                            <TextField
+                                required
+                                id="pwd"
+                                label="Password"
+                                type="password"
+                                variant="outlined"
+                                autoComplete="current-password"
+                                fullWidth
+                                {...register("pwd")}
+                            />
+                        </div>
+
+                        <input type="submit" />
                     </div>
-
-                    <input type="submit" />
                 </form>
-            </div>
-        </Box>
-
+            </Box>
+        </Container>
 
     );
 };
