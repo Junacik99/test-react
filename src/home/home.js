061513ad0219cdabb023bd71758cbd7451bda5cf
martin.takacs <junacik99@gmail.com>
2022-07-07 01:44:38 +0200

    load and display data

--- src/home/home.js
+++ src/home/home.js
@@ -1,29 +1,61 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Home() {
+    const [data, setData] = useState([]);
+
     const loadData = () => {
         fetch('./MOCK_DATA.json',
-        {
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            }
-        })
-        .then(function(response) {
-            console.log(response);
-            return response.json();
-        })
-        .then(function(myJson) {
-            console.log(myJson);
-        });
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            })
+            .then(function (response) {
+                console.log(response);
+                return response.json();
+            })
+            .then(function (myJson) {
+                console.log(myJson);
+                setData(myJson);
+            });
     };
 
     useEffect(() => {
         loadData();
-    },[]);
+    }, []);
+
 
     return (
-        <h1>HOME PAGE</h1>
-
+        <div>
+            <h1>HOME PAGE</h1>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Surname</th>
+                            <th>Age</th>
+                            <th>Disease</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map((patient, index) => {
+                            return (
+                                <tr>
+                                    <td>{patient.id}</td>
+                                    <td>{patient.name}</td>
+                                    <td>{patient.surname}</td>
+                                    <td>{patient.age}</td>
+                                    <td>{patient.disease}</td>
+                                </tr>
+                            )
+                        })}
+                        {console.log(data)}
+                    </tbody>
+                </table>
+            </div>
+        </div>
     )
 }
